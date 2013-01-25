8f519ac46e8b6612da36acae18386b8d5bf32ba8
Dejun Qian <dejun@ruby.cat.pdx.edu>
2013-01-25 15:35:17 -0800

    now passed

--- 120109/Solution.java
+++ 120109/Solution.java
@@ -1,4 +1,4 @@
-mport java.util.*;
+import java.util.*;
 class Coord implements Comparable<Coord> {
     public int index;
     public int value;
@@ -28,14 +28,18 @@ public class Solution {
             heightvec.add(new Coord(i,height[i]));
         }
         Collections.sort(heightvec);
-        Vector<Coord> visited = new Vector<Coord>();
-        visited.add(heightvec.get(0));
+        //Vector<Coord> visited = new Vector<Coord>();
+        //visited.add(heightvec.get(0));
         int capacity = 0;
+        int min = 0;
+        int max = 0;
         for(int i=1; i<heightvec.size(); i++) {
-            visited.add(heightvec.get(i));
-            Collections.sort(visited,Coord.COMPARE_BY_INDEX);
-            Coord c1 = visited.get(0);
-            Coord c2 = visited.get(visited.size()-1);
+            //visited.add(heightvec.get(i));
+            //Collections.sort(visited,Coord.COMPARE_BY_INDEX);
+            min = heightvec.get(i).index<heightvec.get(min).index?i:min;
+            max = heightvec.get(i).index>heightvec.get(max).index?i:max;
+            Coord c1 = heightvec.get(min);
+            Coord c2 = heightvec.get(max);
             int cap = (c2.index-c1.index)*(c1.value<c2.value?c1.value:c2.value);
             if(cap>capacity) {
                 capacity = cap;
